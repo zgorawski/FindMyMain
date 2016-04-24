3249d2ec3313c9fda685cf6cdc8c6c4121fb3246
zbigniew <zbigniew@ZGPC>
2016-04-24 14:27:05 +0200

    Add project files.

---
+++ FindMyMain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindMyMain.Startup))]
+namespace FindMyMain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
