3430b4f60c7dd15a9176af508c326b5b4bc3ca3a
Zbigniew Górawski <Zbigniew Grawski>
2016-04-28 19:18:39 +0200

    Answers model WiP

---
+++ FindMyMain/Model/Answers/ChampionDescription.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyMain.Model.Answers
+{
+    public struct ChampionDescription
+    {
+        public KnownChampion Champion { get; private set; }
+        public string WelcomeQuote { get; private set; }
+
+        public RoleTag Role { get; private set; }
+        public RegionTag Region { get; private set; }
+        public PerkTag[] Perks { get; private set; }
+        public SkinTag[] Skins { get; private set; }
+        
+        public Dictionary<KnownChampion, string> SpecialQuotes { get; private set; }
+
+        public ChampionDescription(KnownChampion champion, string welcomeQuote, RoleTag role, RegionTag region, PerkTag[] perks, SkinTag[] skins, Dictionary<KnownChampion, string> specialQuotes)
+        {
+            Champion = champion;
+            WelcomeQuote = welcomeQuote;
+            Role = role;
+            Region = region;
+            Perks = perks;
+            Skins = skins;
+            SpecialQuotes = specialQuotes;
+        }
+    }
+}
---
+++ FindMyMain/Model/Answers/Tag.cs
@@ -0,0 +1,19 @@
+﻿public enum RoleTag
+{
+    Tank, Fighter, Slayer, Mage, Controller, Marksmen
+}
+
+public enum RegionTag
+{
+    BandleCity, Bilgewater, Damacia, Freljord, Ionia, MountTargon, Noxus, Piltover, ShadowIsles, Shurima, Zaun, Void, Other
+}
+
+public enum PerkTag
+{
+    Animal, Shapeshifter, PetChampion, HealthCostAbilities, Global
+}
+
+public enum SkinTag
+{
+    Project, Hextech, Officer, Headhunter
+}
